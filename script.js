5b4d7d9ec32c9d6b7f2121ac52065b5eeb1de9b8
Andriana <andrianasaffo@gmail.com>
2023-02-02 16:45:25 +0000

    Working on event listener for dropdown menu

--- script.js
+++ script.js
@@ -26,14 +26,50 @@ function search() {
   });
 }
 
-// function makePageForEpisodes(episodeList) {
-//   rootElem.innerHTML = `Got ${episodeList.length} episode(s)`;
-// }
-
-function displayEpisodes(episodes) {
-  document.querySelector(".show-episodes").innerHTML = " ";
+function select(episodes) {
+  const select = document.querySelector("#select");
 
   for (let eachEpisode of episodes) {
+    const option = document.createElement("option");
+    select.appendChild(option);
+
+    option.value = eachEpisode.name;
+
+    const episodeSeason = eachEpisode.season;
+    const episodeNumber = eachEpisode.number;
+    const episodeCode = `S${String(episodeSeason).padStart(2, "0")}: E${String(
+      episodeNumber
+    ).padStart(2, "0")}`;
+
+    option.innerText = `${episodeCode} - ${eachEpisode.name}`;
+  }
+
+  // option.addEventListener("change", (event) => {
+  //   let value = event.target.value;
+  //   const selectedEpisode = episodes.find((episode) => {
+  //     return episode.name.includes(value);
+  //   });
+  //   displayEpisodes(selectedEpisode);
+  // });
+  // selectElement.addEventListener("change", (event) => {
+  //   const result = document.querySelector(".result");
+  //   result.textContent = `You like ${event.target.value}`;
+  // });
+  select.addEventListener("change", (event) => {
+    const selectAllEpisodes = event.target.value;
+    const allEpisodes = getAllEpisodes();
+    const showAll = allEpisodes.find((element) =>
+      element.name.includes(selectAllEpisodes)
+    );
+    console.log(showAll);
+    displayEpisodes(showAll);
+  });
+}
+
+function displayEpisodes() {
+  document.querySelector(".show-episodes").innerHTML = " ";
+
+  for (let eachEpisode of allEpisodes) {
     const episodeSection = document.createElement("section");
     episodeDiv.appendChild(episodeSection);
     episodeSection.setAttribute("class", "box");
@@ -69,9 +105,10 @@ function displayEpisodes(episodes) {
 
 function setup() {
   search();
-  // makePageForEpisodes(allEpisodes);
 
   displayEpisodes(allEpisodes);
+
+  select(allEpisodes);
 }
 
 window.onload = setup;
